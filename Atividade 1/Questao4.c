6bad322f305053f42cdaa02769c49b270ad4dee2
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2023-11-29 13:24:01 -0300

    Create Questao4.c

---
+++ Atividade 1/Questao4.c
@@ -0,0 +1,13 @@
+//Escreva uma funcao que receba dois numeros positivos por parametro e retorne a soma dos N numeros
+//inteiros existentes entre eles
+
+#include <stdio.h>
+
+int main(){
+  int f[n][2];
+  int n, x;
+  for (i = 0; i <= n; i ++){
+    printf("Digite o primeiro valor");
+    scanf("%d", &);
+  }
+}
