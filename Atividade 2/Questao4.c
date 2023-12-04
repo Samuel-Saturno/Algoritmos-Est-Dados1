207f98692aca715ca45fc28f374824c6b5bf0e51
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2023-12-04 14:40:38 -0300

    Update Questao4.c

--- Atividade 2/Questao4.c
+++ Atividade 2/Questao4.c
@@ -9,7 +9,7 @@ Para os cálculos, utilize as funções sqrt e pow do módulo math da biblioteca
 padrão de C. Em seguida, crie a função principal do programa e utilize a função
 calcula_hexagono para calcular a área e o perímetro de um hexágono de lado l
 informado pelo usuário.
-  */
+ */
 
 //respostas: 
 
@@ -17,7 +17,7 @@ informado pelo usuário.
 #include <locale.h>
 #include <math.h>
 
-void calcula_hexagono (float l, float *area, float *perimetro) {
+void CalHex (float l, float *area, float *perimetro) {
     *area = (3 * pow(l, 2) * sqrt(3) / 2);
     *perimetro = 6 * l;
 }
