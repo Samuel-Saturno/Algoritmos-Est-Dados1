d0deb21ecfee1553f25c4fccdbd249005b6f8c6f
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2024-01-29 14:58:07 -0300

    Update ExFguets.c

--- Unidade 2/Aulas/29.01.24/ExFguets.c
+++ Unidade 2/Aulas/29.01.24/ExFguets.c
@@ -14,15 +14,7 @@ int main(void){
     }
 
     c = fgets(fp); //Lê uma linha do arquivo
-    printf("%c\n", c); //Imprime a string
-    
-
-    //Funções para ler dados
-    //int fscanf (FILE* fp, char* formato, ...); //Ler um arquivo em um formato
-    //int fgetc(FILE* fp); //Ler dados do arquivo pegando 1 único caractere que é o primeiro.
-    //char* fgets(char* s, int n, FILE* fp); //Pega uma string e armazena uma variável indicando uma linha e mostrando o ponteiro q vc pegou o arquivo
-
-    //fclose(fp); // Fecha arquivo do sistema operacional e não exclue
+    printf("%s\n", c); //Imprime a string
     
     if(fclose(fp)){
         printf("Arquivo fechado com sucesso\n");
