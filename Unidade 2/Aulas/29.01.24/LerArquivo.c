8bd7cb35ba4796e98d8eac6451e4b42ccbaabfd2
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2024-01-29 14:50:55 -0300

    Create LerArquivo.c

---
+++ Unidade 2/Aulas/29.01.24/LerArquivo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void){
+    FILE* fp;
+    fp = fopen("entrada.txt","rt"); //Cria
+    // fp = fopen("entrada.txt","rt"); Lê o arquivo de Texto
+    if(fp == NULL){
+    printf("Erro na abertura do arquivo\n");
+    exit(1);
+    } else {
+        printf("Arquivo foi criado\n");
+    }
+
+    c = fgetc(fp);
+    printf("%c\n", c);
+    
+    if(fclose(fp)){
+        printf("Arquivo fechado com sucesso\n");
+    }
+
+    return 0;
+}
