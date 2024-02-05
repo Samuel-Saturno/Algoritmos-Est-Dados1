8e15ec605f9ed959d4a40f024487c40dda726f85
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2024-02-05 14:33:44 -0300

    Update aluno.h

--- Unidade 2/Aulas/05.02.24/Tipo Abstrato de Dados/aluno.h
+++ Unidade 2/Aulas/05.02.24/Tipo Abstrato de Dados/aluno.h
@@ -1,1 +1,5 @@
+/*  Definição de novo tipo: Aluno   */
+typedef struct aluno Aluno;
 
+/*  Fução aloca memória para struct aluno, recebe dados via teclado e retorna um ponteiro para Aluno    */
+Aluno* recebe_dados(void);
